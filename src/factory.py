03054030895de2d7c8c28c94762a6a414a310319
bio-boris <bio-boris@github.com>
2025-03-17 16:26:07 -0500

    initial commit

--- src/factory.py
+++ src/factory.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+
+def search():
+    pass
+
+def data():
+    pass
+
+
+def validate(data):
+    pass
 
 
 def create_app() -> FastAPI:
@@ -9,5 +19,23 @@ def create_app() -> FastAPI:
     async def root():
         return {"message": "Hello World"}
 
+    @app.post("/search")
+    async def search(
+        data: dict = Body(
+            example={
+                "query_sequences": [
+                    {"id": "Q1", "sequence": "MKTIIALSY..."},
+                    {"id": "Q2", "sequence": "MKALILVCL..."}
+                ],
+                "similarity_threshold": 0.7,
+                "max_hits": 5,
+                "return_query_embeddings": True,
+                "return_hit_embeddings": False
+            }
+        )
+    ):
+        vd = validate(data)
+        result = search(data)
+        return result
 
     return app
