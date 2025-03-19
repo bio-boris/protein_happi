143af6cf948527f309622e421046a1329a20c3ae
braceal <abrace05@gmail.com>
2025-03-19 13:35:21 -0500

    config

--- src/config.py
+++ src/config.py
@@ -21,10 +21,9 @@ class LLMHomologyApiSettings(BaseSettings):
     AUTH_URL: str
     ADMIN_ROLES: list = ["LLMHomologyAdmin"]
     VCS_REF: str
-    MODEL_DIR: str
 
     # The similarity search configuration
-    GPUS: int = Field(
+    SEARCH_GPUS: int = Field(
         default=0,
         ge=0,
         description="The number of GPUs to use for the similarity search. "
